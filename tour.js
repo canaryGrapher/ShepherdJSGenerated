494694f1d215d266942a2694cfb1567f45a7dcb2
canaryGrapher <yasharyan307@outlook.com>
2023-03-06 10:06:31 +0530

    Initial push

--- tour.js
+++ tour.js
@@ -1,49 +1,49 @@
 
-  alert("ShepherdJS configuration detected. Please wait while the tour is being generated...");
-  var Shepherd = window.Shepherd;
-  var tour = new Shepherd.Tour({
-      useModalOverlay: true,
-      defaultStepOptions: {
-          classes: 'shadow-md bg-purple-dark',
-          scrollTo: true,
-          cancelIcon: {
-              enabled: true
-          }
-      }
-  });
-  tour.addStep({
-      id: "tour_0",
-      title: "Button",
-      text: "This is the button for Exploration",
-      classes: "example-step-extra-class",
-    attachTo: {element: "#section-home-landing > div > button", on: "top"},
-      buttons: [
-        {
-          text: 'Back',
-          action: tour.back
-        },
-        {
-          text: 'Next',
-          action: tour.next
-        }
-      ]
-    })
+alert("ShepherdJS configuration detected. Please wait while the tour is being generated...");
+var Shepherd = window.Shepherd;
+var tour = new Shepherd.Tour({
+  useModalOverlay: true,
+  defaultStepOptions: {
+    classes: 'shadow-md bg-purple-dark',
+    scrollTo: true,
+    cancelIcon: {
+      enabled: true
+    }
+  }
+});
 tour.addStep({
-      id: "tour_1",
-      title: "Home button",
-      text: "This is the home button",
-      classes: "example-step-extra-class",
-    attachTo: {element: "#home", on: "right"},
-      buttons: [
-        {
-          text: 'Back',
-          action: tour.back
-        },
-        {
-          text: 'Next',
-          action: tour.next
-        }
-      ]
-    })
-  // Start the tour
-  tour.start();
+  id: "tour_0",
+  title: "Button",
+  text: "This is the button for Exploration",
+  classes: "example-step-extra-class",
+  attachTo: { element: "#section-home-landing > div > button", on: "top" },
+  buttons: [
+    {
+      text: 'Back',
+      action: tour.back
+    },
+    {
+      text: 'Next',
+      action: tour.next
+    }
+  ]
+})
+tour.addStep({
+  id: "tour_1",
+  title: "Home button",
+  text: "This is the home button f",
+  classes: "example-step-extra-class",
+  attachTo: { element: "#home", on: "right" },
+  buttons: [
+    {
+      text: 'Back',
+      action: tour.back
+    },
+    {
+      text: 'Next',
+      action: tour.next
+    }
+  ]
+})
+// Start the tour
+tour.start();
